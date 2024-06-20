720d8c81accc19905719a1bff2f93e1345bb320d
alvrath <alvrath@gmail.com>
2024-06-20 11:21:51 +0500

    Добавление проверки прохождения тестов (с помощью assert)

--- test_parametrize.py
+++ test_parametrize.py
@@ -13,8 +13,6 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-#@pytest.fixture(scope="session")
-#def test_authorize_stepik(browser, login, password):
 @pytest.mark.parametrize('login, password', [("@gmail.com", "_")])
 @pytest.mark.parametrize('link', [('https://stepik.org/lesson/236895/step/1'), ("https://stepik.org/lesson/236896/step/1"), ("https://stepik.org/lesson/236897/step/1"), ("https://stepik.org/lesson/236898/step/1"), ("https://stepik.org/lesson/236899/step/1"), ("https://stepik.org/lesson/236903/step/1"), ("https://stepik.org/lesson/236904/step/1"), ("https://stepik.org/lesson/236905/step/1")])
 def test_put_a_number_stepik(browser, link, login, password):
